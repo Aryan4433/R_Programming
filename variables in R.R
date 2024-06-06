3bab44fb2b231daf2635fd0f22585971725bbdc4
Aryan Monga <118589541+Aryan4433@users.noreply.github.com>
2024-06-06 14:25:24 +0530

    Add files via upload

---
+++ variables in R.R
@@ -0,0 +1,62 @@
+#R programming :
+#It is a language of programming
+#it is a open source programming language used for statistical analysis
+
+#data types in R :
+  
+#Numeric -- 1,2
+#Character -- "Aryan Monga"
+#Boolean -- True/False
+#Complex -- 2+3i
+
+#creation of Variable:
+
+a = 10
+a
+
+# checking type of a
+class(a)
+
+
+#operators in R
+#Artimetic (+,-,/,*,//)
+a=10
+b=10
+c=a+b
+c
+#Assignment (=,<-1,1->b)
+a=10
+a<-1
+a
+#Relational (>,<,==,!=)
+a=10
+b=20
+a>b
+#Logical (&,|,)
+a=TRUE
+b=FALSE
+a&b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
